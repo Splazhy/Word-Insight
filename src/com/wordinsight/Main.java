7c53e609204071045e2b0f04f1d2eeed2a6dab13
Teeratas Thiangtham <kiwisplash13@gmail.com>
2023-10-25 01:57:18 +0700

    add Dockerfile with args passing arch

--- src/com/wordinsight/Main.java
+++ src/com/wordinsight/Main.java
@@ -2,6 +2,9 @@ package com.wordinsight;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No words given, do nothing, exiting...");
+        }
         for (int i = 0; i < args.length; i++) {
             String jsonResponse = DictionaryFetcher.get(args[i]).get();
             DictionaryParser parser = new DictionaryParser();
